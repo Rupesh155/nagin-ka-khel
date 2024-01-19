24b980493dda1a325dc41f28790684d4ae6bd95b
Rupesh <rupesh@Rupeshs-MacBook-Air.local>
2024-01-19 15:13:30 +0530

    snake

---
+++ index.js
@@ -0,0 +1,1258 @@
+
+
+
+//     // undefined vs null
+
+//     // let user=null
+//     // console.log(user)
+//     // if(0){
+//     //     console.log('hello')
+//     // }
+//     // else{
+//     //     console.log('hiii')
+//     // }
+
+//     // let isBool=true
+//     // isBool?console.log('hello'):console.log('hii')
+
+//     // let arr=[1,2,3,'one', true]
+
+//     // console.log('hello')
+// //     function sum(){   
+// //        console.log('hello')
+// //     }
+// //     sum()
+// //     const add= () =>  {
+// //         console.log('hii')
+// //     }
+// //  add()
+
+           
+// // let arr=[1,2,3,4,5,6,7,8]
+
+// //   let filter1=  arr.filter((a,b,c)=>{
+// //     return a>3
+
+
+// // })
+// // console.log(filter1)
+
+// //  let newA=  arr.forEach( (a,b,c)=>{
+// //     console.log(a*2)
+// // }  )
+// // console.log(newA)
+// //  let newa1=  arr.map((a,b,c)=>{
+// //     // console.log(a,b,c)
+// //     return a>3
+
+// // })
+// // console.log(newa1)
+
+// // for(let i in arr){
+// //     // console.log(i)
+// // }
+// // for(let i of arr){
+// //     console.log(i)
+// // }
+// // for(let i=0;i<5;i++){
+
+
+// // }
+           
+
+
+
+    
+
+            
+
+// // undefined VS null
+
+// // let user='user'
+// // console.log(user)
+
+
+
+// // if(true){
+// //     console.log('hello')
+// // }
+// // else{
+// //     console.log('hello hiiiiii')
+// // }
+// //   let user=true
+// //   user?console.log('hello'):console.log('hiii byteee')
+
+// //    let arr=[1,2,3,4,5,6,6,7,8,8]
+// //         let new1=   arr.reduce((a,b,c,d)=>{
+// //              return a+b
+// //             })
+// //         console.log(new1)
+
+     
+
+// // let arr3=[1,2,3,4,5,6]
+// //        let new2=   arr3.reduce((a,b,c,d)=>{
+// //             return a+b
+     
+
+// //           })
+// //           console.log(new2)
+
+
+
+// // Object
+
+
+// // function add(a){
+// //     console.log(a.name)
+// // }
+
+// // let  students ={
+// //     name:'abc',
+// //     age:''
+// //     isPass:true
+// // }
+// // add(students)
+// // students.name='xyz'
+// // let obj={
+// //     user:'modi',
+// //     lastName:'ji',
+// //     sum:function(){
+// //         console.log(this.user+this.lastName)
+// //     }
+// // }
+// // obj.sum()
+
+
+
+// // let arr=[{id:1,name:'roohfza'}
+// // ,
+
+// // {id:2,name:"jyoti"}]
+//             //    arr.map((k)=>{
+//             //     console.log(k.id)
+//             //    })
+// // console.log(arr.id)
+
+// // JSON()
+
+
+
+
+
+// // let arr=[1,2,3,4,56]
+// // console.log(arr[0].id)
+// //   arr.push(10)
+// // // arr.pop()
+// // // arr.unshift(0)
+// // //   console.log(arr)
+
+
+// // let arr1=[
+// //     {
+// //         id:1,
+// //         name:'naam'
+// //     },
+// //     {
+// //         id:2,
+// //         name:'nam2'
+
+// //     }
+// // ]
+
+
+
+// // arr1.map((a)=>{
+// //     console.log(a.id)
+
+// // })
+
+// //      let obj={  
+
+// //         id:1,
+// //         user:"hello",
+// //         isPass:true
+
+// //      }
+// // obj.user='hiiii'
+
+//     //  let arr=[{id:1,user:'hello'},{id:2,user:'hiii'}]
+     
+//     //  arr.map((k)=>{
+//     //     console.log(k.id)
+
+//     //  })
+
+//     // let arr=[1,2,3,4,5,5 ]
+//     // arr.push(7)
+//     // // arr.pop()
+//     // arr.unshift(10)
+//     // // arr.shift()
+//     // console.log(arr)
+
+
+//         //    function add(a){
+//         //     console.log(a.id)
+
+//         //    }
+
+//         //    let obj={
+//         //     id:1,
+//         //     userName:'hello'
+//         //    }
+
+//         //    add(obj)
+//         // let obj={
+//         //     firstName:'hello',
+//         //     lastName:'hii',
+//         //     fullName:function(){
+//         //    console.log(this)
+//         // //     }
+//         // // }
+//         // // obj.fullName()
+
+
+
+
+
+
+//         // let user='helloooo'
+//         // function add(){
+//         //     return 5
+//         //     let num=5
+//         //     console.log(num)
+//         // }
+//         // console.log(user)
+//         //  let a=  add()
+//         //  console.log(a)
+
+
+
+
+//         //  function outer(){
+//         //     let num=5
+//         //     inner()
+//         //     function inner(){
+//         //         console.log(num)
+
+//         //     }
+//         //  }
+//         //  outer()
+
+
+
+
+
+
+
+// //   idher dekho
+
+
+// // let user='abhi'
+// // function add(a){
+   
+// //     console.log(a)
+// // }
+// // add(5)
+// // console.log(user)
+
+
+
+// // function outer(){
+  
+// // inner()
+// // var num=5
+
+// //     function inner(){
+// //         console.log(num)
+
+// //     }
+// // }
+
+// // outer()
+
+
+
+
+// // console.log('nhi chalega')
+
+
+
+
+
+
+// // console.log(a)
+// // let a=5
+
+
+// // console.log('hello')
+
+// // {
+// //     console.log('hiii')
+// // }
+
+
+
+// // var a=5
+// // if(true){
+// //     var a=6
+// //     console.log(a)
+// // }
+// // console.log(a)
+
+
+// // {
+// //     let a=5
+// //     console.log(a)
+// // }
+// // console.log(a) 
+
+
+
+
+// // higher order function
+
+// //  function outer(a){
+// //     console.log('outer')
+// //     a()
+
+// //  }
+// //   function inner(){
+// //     console.log('inner')
+// //   }
+// //  outer(inner)
+
+
+
+// // function outer(){
+// //     console.log('outer')
+// //     function inner(){
+// //         console.log('inner')
+
+// //     }
+// //    return inner
+// // }
+// //  let inner=   outer()
+// //  inner()
+// //    let arr=[1,2,3,4,'hello','hiii', true, false]
+
+
+
+
+//     // Hoisting
+// //  console.log(a)
+// //    var a=5
+// // let b
+// // console.log(b)
+// // b=5
+
+// // function outer(){
+// //     console.log('hello')
+ 
+// // }
+// // outer()
+// // add()
+// // f add=()=>{
+// //     console.log('hiiiii')
+
+// // }
+
+// // add()
+// // function add(){
+
+// // }
+// // let b=10
+// // {
+// //     // var a=5
+// //     let b=5
+// //     // console.log(a)
+// //     console.log(b)
+// // }
+// // console.log(b)
+
+
+      
+// // Higher order function
+
+
+
+
+// // function outer(a){
+// //     console.log('hello')
+// //     a()
+// //     // console.log(a)
+// // }
+
+// // function inner(){
+// //     console.log('hiiiiii')
+// // }
+// // outer(inner)
+
+// // function outer(){
+// //     // console.log('')
+// //     console.log('outer')
+// //     function inner(){
+// //         console.log('hiiii')
+// //     }
+// //     return inner
+// // }
+// //   let inner= outer()
+// //   inner
+// // // console.log(a())
+
+
+// //   function outer(){
+// //     console.log('hello')
+// //     function inner(){
+// //         console.log('inner')
+
+// //     }
+// //     return inner
+// //   }
+// //   outer()
+
+
+
+// // let arr=[1,2,3,4,5,true, false,'hello','hiiiii']
+// //      function getString(item){
+// //         return typeof item==='string'
+// //      }
+// //      function getNumber(item){
+// //         return typeof item==='number'
+// //      }
+// //      function getBool(item){
+// //         return typeof item==='boolean'
+// //      }
+// //      function get(arr,fn){
+// //         let res=[]
+// //         for(let item of arr){
+// //             if(fn(item)){
+// //                 res.push(item)
+// //             }
+// //         }
+// //         return res
+// //      }
+// // console.log(get(arr,getString))
+// // console.log(get(arr,getNumber))
+// // console.log(get(arr,getBool))
+
+
+
+
+
+
+
+
+
+// // function get(arr){
+// //     let res=[]
+// //     for(let item of arr){
+// //         if(typeof item==='string'){
+// //             res.push(item)
+// //         }
+     
+// //     }
+// //     return res
+
+// // }
+// // function getNumber(arr){
+// //     let res=[]
+// //     for(let item of arr){
+// //         if(typeof item==='number'){
+// //             res.push(item)
+// //         }
+     
+// //     }
+// //     return res
+
+// // }
+
+// // function getBool(arr){
+// //     let res=[]
+// //     for(let item of arr){
+// //         if(typeof item==='boolean'){
+// //             res.push(item)
+// //         }
+     
+// //     }
+// //     return res
+
+// // }
+// // console.log(get(arr))
+// // console.log(getNumber(arr))
+// // console.log(getBool(arr))
+
+
+
+
+//     // callBack  function
+
+//     // function callBack(num, fn){
+//     //     return fn(num)
+
+//     // }
+//     // function boolean(num){
+//     //      return  typeof num ==='boolean'
+//     // }
+
+//     // console.log(callBack(true,boolean))
+
+
+
+
+
+//     //   function outer(){
+//     //     let user='hello'
+//     //      function inner(){ 
+//     //         console.log(user)
+//     //      }
+//     //     //  inner()
+//     //     return inner
+
+//     //   }
+//     //  let inner=  outer()
+//     //  inner()
+  
+//     var arr1=[1,2,3,4,5,'string','hello', true,false]
+
+//         function getString(item){
+//             return typeof item ==='string'
+//         }
+//         function getNumber(item){
+//             return typeof item ==='number'
+//         }
+//         function getBool(item){
+//             return typeof item ==='boolean'
+//         }
+//         function get(arr1,fn){
+//             let res=[]
+//             for(let item of arr1){
+//                 if(fn(item)){
+//                     res.push(item)
+
+//                 }
+//             }
+//             return res
+
+//         }
+
+//         // console.log(get(arr1,getString))
+//         // console.log(get(arr1,getNumber))
+//         // console.log(get(arr1,getBool))
+
+//         //     function outer(){
+//         //         let user='yhi aayega'
+//         //         function inner(){
+//         //             console.log(user)
+//         //         }
+//         //         // inner()
+//         //         return inner
+//         //     }
+//         // let a=  outer()
+//         // a()
+         
+
+//         var x = 10; 
+//         function foo() { 
+//             console.log(x);
+//             var x = 20; 
+//         } 
+//         foo();
+
+
+//         var x = 'abc'
+//         console.log(x[0]);
+//        x[0] = 'z'; 
+//       console.log(x[0]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         // function getString(arr1){
+//         //     let res=[]
+//         //     for(let item of arr1){
+//         //         if(typeof item =='string')
+//         //         {
+//         //             res.push(item)
+//         //         }
+//         //     }
+//         //     return res
+
+//         // }
+//         // function getNumber(arr1){
+//         //     let res=[]
+//         //     for(let item of arr1){
+//         //         if(typeof item =='number')
+//         //         {
+//         //             res.push(item)
+//         //         }
+//         //     }
+//         //     return res
+
+//         // }
+//         // function getBool(arr1){
+//         //     let res=[]
+//         //     for(let item of arr1){
+//         //         if(typeof item =='boolean')
+//         //         {
+//         //             res.push(item)
+//         //         }
+//         //     }
+//         //     return res
+
+//         // }
+//         // console.log(getString(arr1))
+//         // console.log(getNumber(arr1))
+//         // console.log(getBool(arr1))
+
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+     
+
+
+            
+
+       
+    
+
+
+
+     
+
+
+
+     
+
+
+
+
+
+
+
+
+
+// // let new2=      arr.find((a,b,c)=>{
+// //     return a>2
+// //       })
+//     //   console.log(new2)
+//         // let new1=      arr.filter((a,b,c)=>{
+//         //     return a>2
+
+//         //       })
+//         //       console.log(new1)
+
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+
+        
+
+
+// //     let new2= arr.find((a)=>{
+// //         return a>2
+// //      })
+// //      console.log(new2)
+
+// //   let new1=    arr.filter((a,b,c)=>{
+// //     return a>2
+
+// //       })
+// //       console.log(new1)
+
+
+
+
+
+// //      let new1= arr.map((a)=>{
+// //     // console.log(a)
+// //     return a
+
+// //    })
+// //    console.log(new1)
+
+// //    for(let i  in arr){
+// //     console.log(i)
+
+// //    }
+
+// //    for(let i of arr){
+// //     console.log(i)
+// //    }
+
+// //   arr.forEach( (c,b,a)=>{
+// //     // return a
+// //     console.log(c*2)
+// //    }  )
+// //    console.log(new1)
+
+//     //  console.log('hello')
+
+//     //   function add(){
+//     //     console.log('byee')
+//     //     return 'hiii'
+//     //     console.log('hello')
+//     //   }
+
+//     // let user=  add()
+//     // console.log(user)
+//     //   add()
+
+//     // const add=(a)=>{
+//     //     console.log('hello')
+//     // }
+//     // add(5)
+
+
+
+
+
+
+
+
+
+
+
+
+// //    for(int i=0;i<5;i++){
+
+// //    }
+
+// // for(let i =0;i<5;i++){
+
+// // }
+
+
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
+
+// ctx.fillRect(0,0,50,50)
+
+let boardHeight = 400;
+let boardWidth = 1000;
+let square = 50;
+
+let snakeCells = [[0,0]];
+
+let direction = 'right';
+
+let gameOver = false;
+
+let foodCell = generateRandomCell();
+
+let score = 0;
+
+document.addEventListener('keydown', function(event) {
+  console.log(event,"rrr")
+  if (event.key === 'ArrowLeft') {
+    direction = 'left';
+  } else if (event.key === 'ArrowDown') {
+    direction = 'down';
+  } else if (event.key === 'ArrowRight') {
+    direction = 'right';
+  } else {
+    direction = 'up';
+  }
+})
+
+let intervalId = setInterval(function() {
+  update();
+  draw();
+}, 100);
+[1,2,3,4,5]
+function update() {
+  let headX = snakeCells[snakeCells.length - 1][0];
+  let headY = snakeCells[snakeCells.length - 1][1];
+
+  let newX
+  let newY
+
+  if (direction === 'right') {
+    newX = headX + square;
+    newY = headY;
+
+    if (newX === boardWidth) {
+      gameOver = true;
+    }
+    
+  } else if (direction === 'left') {
+    newX = headX - square;
+    newY = headY;
+
+    if (newX < 0) {
+      gameOver = true;
+    }
+
+  } else if (direction === 'up') {
+    newX = headX;
+    newY = headY - square;
+
+    if (newY < 0) {
+      gameOver = true;
+    }
+
+  } else {
+    newX = headX;
+    newY = headY + square;
+
+    if (newY === boardHeight) {
+      gameOver = true;
+    }
+  }
+
+  snakeCells.push([newX, newY]);
+ 
+  if (foodCell[0] === headX && foodCell[1] === headY) {
+    foodCell = generateRandomCell();
+    score += 1;
+  } else {
+    snakeCells.shift();
+  
+  }
+}
+
+function draw() {
+
+  if (gameOver === true) {
+    clearInterval(intervalId);
+    ctx.fillStyle = 'red';
+    ctx.font = '40px sans-serif';
+    ctx.fillText('Game over', 50, 150);
+    return;
+  }
+
+  ctx.clearRect(0, 0, boardWidth, boardHeight);
+  for (let cell of snakeCells) {
+    ctx.fillStyle = 'yellow';
+    ctx.fillRect(cell[0], cell[1], square, square);
+    // ctx.fillStyle = 'red';
+    // ctx.strokeRect(cell[0], cell[1], square, square);
+  }
+
+  ctx.fillStyle = 'red';
+  ctx.fillRect(foodCell[0], foodCell[1], square, square);
+  // ctx.fillStyle = 'yellow';
+
+  // draw score
+  // ctx.fillStyle = 'white';
+  ctx.font = '20px sans-serif';
+  ctx.fillText(`Score: ${score}`, 50, 50);
+}
+// draw()
+
+function generateRandomCell() {
+  return [
+    Math.round((Math.random()*(boardWidth ))/square)*square,
+    Math.round((Math.random()*(boardHeight))/square)*square
+  ]
+}
+
+// console.log(generateRandomCell())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         
+
+
+  // let X= Math.random()*1000
+  // console.log(X)
+
+
+
+       
+           
+
+
+
+
+          
+           
+        
+
+
+
+
+
+
+
+
+
+
+
+ 
+  
+
+
+
+
+
+
+     
+
+
+
+
+
+
+           
+
+
+
+
+                       
+               
+
+     
+
+
+
+
+
+
+    
+
+
+         
+     
+           
+
+
+       
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+       
+
+
+              
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// HTML +JS
+// D   O   M
+    // let h1=   document.getElementById('one')
+    // h1.style.color='red'
+    // console.log(h1.innerText)
+
+
+//  let h1=   document.getElementsByTagName('h1')
+//  for(let  item of h1){
+//     item.style.color='red'
+//  } 
+//  console.log(h1)
+
+
+//  let a= document.querySelector('a')
+// let h1=   document.getElementsByClassName('one')
+// console.log(a.getAttribute('href'))
+// a.setAttribute('href','abccccccc')
+// console.log(a.getAttribute('href'))
+
+   
+// const arr = [
+//     'https://images.unsplash.com/photo-1662330287683-6032da28889c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=900&q=60', 
+//     'https://images.unsplash.com/photo-1659535907680-0e219b46c01d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=900&q=60', 
+//     'https://images.unsplash.com/photo-1660899599007-771f97039eee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNHx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
+//     'https://images.unsplash.com/photo-1662324580989-591a589c5e10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyOHx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60'
+//   ]
+  
+// //   const imageEl = document.querySelector('img');
+  
+// //   let num = 0;
+  
+// //   setInterval(function() {
+// //     imageEl.setAttribute('src', arr[num]); 
+// //     num = (num + 1) % arr.length;
+// //   }, 2000);
+
+      
+
+   
+
+// // let obj={
+// //     id:1,
+// //     user:'hello',
+// //     print:function(){
+// //         console.log(this.user)
+// //     }
+// // }
+// // // obj.print()
+
+// // console.log(obj.toString())
+
+
+
+
+
+// // let arr=[1,2,3,4,5,6]
+
+// // arr.map((a)=>{
+// //     console.log(a)
+
+// // })
+
+
+
+// let obj={
+
+// }
+
+
+//        function outer(user,lastName){
+//         this.userName=user,
+//         this.userLastName=lastName
+
+//        }
+
+//      let user=  new  outer('hello', 'hiiii')
+//      console.log(user)
+
+
+
+
+
+
+
+
+// let arr=[1,2,3,4,5,6]
+
+// arr.map((a)=>{
+
+// })
+
+     
+   
+
+
+
+// let obj={
+//     id:1,
+//     user:'yachnaaa hppy b',
+//     print:function(){
+//         console.log(this.user)
+//     }
+// }
+// // obj.print()
+// console.log(obj.toString())
+//    function outer(){
+//     this.userName='hello',
+//     this.lastName='hiii'
+//    }
+//    let user=    new  outer()
+//    console.log(user)
+
+
+
+// // Select input element
+// const titleEl = document.getElementById('title');
+// // select list element
+// const listEl = document.getElementById('list');
+
+// // select button element
+// const addButtonEl = document.getElementById('add-button');
+
+// // adding "click" event listener on button element
+// addButtonEl.addEventListener('click', function() {
+
+//   // getting the value of the input field
+//   const titleText = titleEl.value;
+  
+//   // creating and 'li' element and adding inner text
+//   const li = document.createElement('li');
+//   li.innerText = titleText;
+
+//   // add the newly created li element to the list
+//   listEl.appendChild(li);
+
+//   // clear the input field after adding li to the list
+//   titleEl.value = '';
+
+//   // remove li when it is clicked
+//   li.addEventListener('click', function() {
+//     li.remove();x
+//   })
+
+// })
+
+
+
+
+ let inp=  document.querySelector('input')
+  let btn=    document.querySelectorAll('button')
+  for(let button of btn){
+    button.addEventListener('click',(e)=>{
+     let text=    e.target.innerText
+     if(text==='C'){
+      inp.value=''
+     }
+     else if(text === '='){
+     inp.value=   eval(inp.value)
+   
+     }
+     else{
+      inp.value+=text
+     }
+    //  console.log(text)
+
+    })
+
+  }
+
+
+
+
